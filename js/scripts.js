5e2e57503104477d54646240e2dc232b6bbca8b2
JulianaPereiraLacerda-dev <julianapereiralacerda62@gmail.com>
2026-03-30 17:08:54 -0300

    :construction: subindo alterações feita no  JS e CSS

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,8 @@ const produtos = [
 ];
 
 const campoBusca = document.getElementById("campoBusca");
-const ordenacao = document.getElementById("ordencao");
-const listaProdutos = document.getElementById("listaProdutos");
+const ordenacao = document.getElementById("ordenacao");
+const listaProdutos = document.getElementById("lista-produtos");
 
 function mostrarProdutos(){
 
@@ -20,4 +20,77 @@ function mostrarProdutos(){
     //Percorre a lista de produtos e valida se existe o nome buscado dentro da lista de produtos.
     //include -> valida se existe o texto buscado dentro da lista de produtos 
     let produtosFiltrados = produtos.filter(produto => produto.nome.toLowerCase().includes(textoBusca));
+
+    //Filtro de ordenação (select)
+
+    //Verifica se a opção selecionada é "Nome A-Z"----------------------------------------------------------------
+    if(ordenacao.value === "nome-az"){
+        produtosFiltrados.sort((a, b) => {
+
+            //localeCompare retorna:
+            //negativo -> A vem antes
+            //positivo -> B vem antes
+            //0 -> são iguais
+            return a.nome.localeCompare(b.nome);
+        })
+    }
+
+    //Verifica se a opção é "nome-za"---------------------------------------------------------------------------------
+    if(ordenacao.value === "nome-za"){
+        produtosFiltrados.sort((a, b) => {
+            return b.nome.localeCompare(a.nome);
+        })
+    }
+
+    // Verifica se "menor preço"-------------------------------------------------------------------------------------
+    if(ordenacao.value === "preco-menor"){
+        produtosFiltrados.sort((a, b) => {
+            // Subtrai os valores e se der negativo -> a vem antes do b
+            // Subtrai os valores e se der positivo  -> b vem antes do a
+            return a.preco - b.preco;
+        })
+    }
+
+    //Verifica se é "maior preço"---------------------------------------------------------------------------------
+    if(ordenacao.value === "preco-maior"){
+        produtosFiltrados.sort((a, b) => {
+
+            //inverte a subtração 
+            return b.preco - a.preco;
+        })
+    }
+
+
+
+    //Limpar a tela 
+    listaProdutos.innerHTML = "";
+
+
+    //Se não encontrar nenhum produto
+    if(produtosFiltrados.length === 0){
+        listaProdutos.innerHTML = "<p class='mensagem'>Nenhum produto encontrado.</p>"
+        return;
+    }
+
+    //cria o card de produto para cada produto
+    for(let i = 0; i < produtosFiltrados.length; i++){
+        let produto = produtosFiltrados[i];
+
+        listaProdutos.innerHTML += 
+            "<div class='card'>" +
+                 "<h2>" +produto.nome + "</h2>" +
+                 "<p class='cat'> Categoria:"+produto.categoria + "</p>" +
+                 "<p class='preco'> R$" +produto.preco.toFixed(2).replace(".", ",") + "<p>" 
+            " </div>";
+    }
+
 }
+
+//iniciando na tela, chamando a função de mostrar produtos
+campoBusca.addEventListener("input", () => {
+    mostrarProdutos();
+})
+ordenacao.addEventListener("change", () =>{
+    mostrarProdutos();
+})
+mostrarProdutos();
